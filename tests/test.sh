4e60dd6d31f011bc6cedc71586301632c37a8d9c
Ricardo Sanz <ricardo@metadrop.net>
2018-05-01 13:24:26 +0200

    Do not wrap packages var in quotes or apt taks it as one package name.

--- tests/test.sh
+++ tests/test.sh
@@ -175,7 +175,7 @@ function prepare_docker_container() {
 
   log_notice 2 "Installing Nginx using apt"
   log_cmd "docker exec $container_id apt-get install $packages  -y"
-  $simcom docker exec $container_id apt-get install "$packages"  -y
+  $simcom docker exec $container_id apt-get install $packages  -y
 
   log_notice 2 "Adding container IP to /etc/hosts"
   container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
